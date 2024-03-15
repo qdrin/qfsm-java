a955dd038f101d37b6281139a7b0a92ecf8fda67
Kudrin Dmitry <dmitry.kudrin@nexign.com>
2024-03-15 17:00:31 +0300

    2 task configuration starts to work

--- src/main/java/org/qdrin/qfsm/machine/states/PriceActiveEntry.java
+++ src/main/java/org/qdrin/qfsm/machine/states/PriceActiveEntry.java
@@ -1,19 +1,52 @@
 package org.qdrin.qfsm.machine.states;
-import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.action.Action;
+
+import com.github.kagkarlsson.scheduler.Scheduler;
+import com.github.kagkarlsson.scheduler.SchedulerClient;
+import com.github.kagkarlsson.scheduler.task.Task;
+import com.github.kagkarlsson.scheduler.task.helper.OneTimeTask;
+import com.github.kagkarlsson.scheduler.task.helper.RecurringTask;
+import com.github.kagkarlsson.scheduler.task.helper.Tasks;
+import com.github.kagkarlsson.scheduler.task.schedule.FixedDelay;
+import org.qdrin.qfsm.tasks.ScheduledTasks;
+import org.qdrin.qfsm.tasks.ScheduledTasks.TaskContext;
+
+import java.util.function.Consumer;
+
+import javax.sql.DataSource;
+
 import org.qdrin.qfsm.machine.actions.SignalAction;
+import org.qdrin.qfsm.model.*;
 
 import lombok.extern.slf4j.Slf4j;
 
 
 @Slf4j
 public class PriceActiveEntry implements Action<String, String> {
+
+  @Autowired
+  SchedulerClient schedulerClient;
+  
   @Override
   public void execute(StateContext<String, String> context) {
     log.debug("PriceActiveEntry started. event: {}", context.getEvent());
     new SignalAction("prolong").execute(context);
-    context.getStateMachine().getExtendedState().getVariables().remove("nextPrice");
+    Product product = context.getExtendedState().get("product", Product.class);
+    ProductPrice nextPrice = context.getStateMachine().getExtendedState().get("nextPrice", ProductPrice.class);
+    if(! context.getEvent().equals("complete_price")) {
+      product.setActiveEndDate(nextPrice.getNextPayDate());
+      Consumer<TaskContext> priceEndedFunc = ScheduledTasks::startPriceEndedTask;
+      Consumer<TaskContext> changePriceFunc = ScheduledTasks::startChangePriceTask;
+      TaskContext ctx = new TaskContext();
+      ctx.id = product.getProductId();
+      ctx.schedulerClient = schedulerClient;
+      ctx.wakeAt = product.getActiveEndDate().toInstant();
+      priceEndedFunc.accept(ctx);
+      changePriceFunc.accept(ctx);
+      context.getStateMachine().getExtendedState().getVariables().remove("nextPrice");
+    }
   }
 }
--- src/main/java/org/qdrin/qfsm/tasks/ScheduledTasks.java
+++ src/main/java/org/qdrin/qfsm/tasks/ScheduledTasks.java
@@ -1,13 +1,19 @@
 package org.qdrin.qfsm.tasks;
 
 import java.time.Duration;
+import java.time.Instant;
+
+import javax.sql.DataSource;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.github.kagkarlsson.scheduler.task.Task;
-import com.github.kagkarlsson.scheduler.task.schedule.FixedDelay;
+import com.github.kagkarlsson.scheduler.Scheduler;
+import com.github.kagkarlsson.scheduler.SchedulerClient;
+import com.github.kagkarlsson.scheduler.task.*;
+import com.github.kagkarlsson.scheduler.task.helper.OneTimeTask;
 import com.github.kagkarlsson.scheduler.task.helper.Tasks;
 import com.github.kagkarlsson.scheduler.task.schedule.Schedule;
 
@@ -16,21 +22,40 @@ import lombok.extern.slf4j.Slf4j;
 @Configuration
 @Slf4j
 public class ScheduledTasks {
+  public static class TaskContext {
+    public SchedulerClient schedulerClient;
+    public String id;
+    public String event;
+    public Instant wakeAt;
+  }
 
-  private Schedule fixedDelay(Duration interval) {
-    return FixedDelay.of(interval);
+  public static final TaskWithoutDataDescriptor price_ended_TASK = new TaskWithoutDataDescriptor("price_ended");
+  public static final TaskWithoutDataDescriptor change_price_TASK = new TaskWithoutDataDescriptor("change_price");
+
+  public static void startPriceEndedTask(TaskContext taskContext) {
+    taskContext.schedulerClient.schedule(price_ended_TASK.instance(taskContext.id), taskContext.wakeAt);
+  }
+
+  public static void startChangePriceTask(TaskContext taskContext) {
+    taskContext.schedulerClient.schedule(change_price_TASK.instance(taskContext.id), taskContext.wakeAt);
   }
 
   @Bean
-  Duration eagerInterval() {
-    return Duration.ofSeconds(10);
-  }
-
-  @Bean
-  Task<Void> dumbLogging(Duration eagerInterval) {
+  Task<Void> priceEndedTask() {
     return Tasks
-        .recurring("dumb-logging", fixedDelay(eagerInterval))
-        .execute((instance, ctx) -> log.info("Hi, I log this statement every {}", eagerInterval));
+        .oneTime(price_ended_TASK)
+        .execute((instance, ctx) -> {
+            log.info("price_ended_TASK run. instance: {}, ctx: {}", instance, ctx);
+    });
+  }
+
+  @Bean
+  Task<Void> startChangePriceTask() {
+    return Tasks
+        .oneTime(change_price_TASK)
+        .execute((instance, ctx) -> {
+            log.info("change_price_TASK run. instance: {}, ctx: {}", instance, ctx);
+    });
   }
 }
 
