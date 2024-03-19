4cd57a2dccc7605a613df1dac3068d5976a1b856
Kudrin Dmitry <dmitry.kudrin@nexign.com>
2024-03-19 15:36:44 +0300

    QStateMachineService starts to work

--- src/main/java/org/qdrin/qfsm/machine/actions/CustomTransition.java
+++ src/main/java/org/qdrin/qfsm/machine/actions/CustomTransition.java
@@ -23,17 +23,19 @@ public class CustomTransition {
         // @EventHeaders Map<String, Object> headers,
         StateContext<String, String> context
         ) {
-    State<String, String> state = context.getSource();
+    State<String, String> source = context.getSource();
+    String targetId = context.getTarget().getId();
+    String sourceId = source == null ? "null" : source.getId();
     StateMachine<String, String> machine = context.getStateMachine();
     AbstractState<String, String> mstate = (AbstractState<String, String>) machine.getState();
     ExtendedState extendedState = context.getExtendedState();
     int count = (int) extendedState.getVariables().getOrDefault("transitionCount", 0) + 1;
     extendedState.getVariables().put("transitionCount", count);
-    log.debug("registerTransition state: {}, transitionCount: {}", state.getId(), count);
+    log.debug("registerTransition {} -> {}, transitionCount: {}", sourceId, targetId, count);
     
     // TODO: Check condition function for it may cause double action call
-    if(mstate.isComposite()) { 
-      Collection<Function<StateContext<String, String>, Mono<Void>>> exitActions = state.getExitActions();
+    if(source != null && mstate.isComposite()) { 
+      Collection<Function<StateContext<String, String>, Mono<Void>>> exitActions = source.getExitActions();
       for(Function<StateContext<String, String>, Mono<Void>> action: exitActions) {
         log.debug("registerTransition run exitAction: {}", action.getClass().getSimpleName());
         action.apply(context).block();
