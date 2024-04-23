e409f6658b7656d33cedac297f04359bb8534ef1
Kudrin Dmitry <dmitry.kudrin@nexign.com>
2024-04-23 11:08:03 +0300

    Fix simple product bundle add to collection, add activation_aborted.json

--- test/send.sh
+++ test/send.sh
@@ -1,2 +1,2 @@
-curl -H "Content-Type: application/json" -vd @activation_started.json http://localhost:8080/qfsm/v1/event | jq ''
+curl -H "Content-Type: application/json" -vd @$1 http://localhost:8080/qfsm/v1/event | jq ''
 
