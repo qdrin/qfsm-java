10fcf6a6de4e2b6e61c29b4ebabe880778eba547
qdrin <dqdrin@gmail.com>
2024-04-27 08:18:03 +0300

    Add accessor and SimpleStateSet test

--- src/test/java/org/qdrin/qfsm/fsm/StateMachineTest.java
+++ src/test/java/org/qdrin/qfsm/fsm/StateMachineTest.java
@@ -2,10 +2,22 @@ package org.qdrin.qfsm.fsm;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.StateMachineContext;
+import org.springframework.statemachine.access.StateMachineAccess;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.OffsetDateTime;
+import java.util.function.Consumer;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.qdrin.qfsm.ProductBuilder;
+import org.qdrin.qfsm.model.Product;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.statemachine.test.StateMachineTestPlan;
 import org.springframework.statemachine.test.StateMachineTestPlanBuilder;
 
@@ -20,21 +32,37 @@ public class StateMachineTest {
   @BeforeEach
   public void setup() throws Exception {
     machine = stateMachineFactory.getStateMachine();
-    for(int i = 0; i < 10; i++) {
-      if(machine.getState() != null) {
-        break;
-      }
-      Thread.sleep(200);
-    }
   }
 
   @Test
   public void testInitial() throws Exception {
     StateMachineTestPlan<String, String> plan =
         StateMachineTestPlanBuilder.<String, String>builder()
+          .defaultAwaitTime(2)
           .stateMachine(machine)
           .step()
-              .expectState("Entry")
+              .expectState("Aborted")
+              .and()
+          .build();
+        plan.test();
+  }
+
+  @Test
+  public void testSimpleStateSet() throws Exception {
+    var accessor = machine.getStateMachineAccessor();
+    StateMachineContext<String, String> context = new DefaultStateMachineContext<String,String>("Aborted", null, null, null);
+    Consumer<StateMachineAccess<String, String>> access;
+    access = arg -> {
+      arg.resetStateMachineReactively(context).block();
+    }; 
+    accessor.doWithAllRegions(access);
+
+    StateMachineTestPlan<String, String> plan =
+        StateMachineTestPlanBuilder.<String, String>builder()
+          .defaultAwaitTime(2)
+          .stateMachine(machine)
+          .step()
+              .expectState("Aborted")
               .and()
           .build();
         plan.test();
@@ -42,19 +70,25 @@ public class StateMachineTest {
 
   @Test
   public void testActivationStartedSuccess() throws Exception {
+    Product product = new ProductBuilder("simpleOffer1", "", "simple1-price-active").build();
+    machine.getExtendedState().getVariables().put("product", product);
     StateMachineTestPlan<String, String> plan =
         StateMachineTestPlanBuilder.<String, String>builder()
+          .defaultAwaitTime(2)
           .stateMachine(machine)
           .step()
               .expectState("Entry")
               .and()
           .step()
-              .sendEvent("activation_started")
+              .sendEvent(MessageBuilder.withPayload("activation_started")
+                  .setHeader("product", product)
+                  .setHeader("datetime", OffsetDateTime.now())
+                  .build())
               .expectState("PendingActivate")
               .expectStateChanged(1)
               .and()
           .build();
-        plan.test();
+    plan.test();
+    assertEquals(product.getStatus(), "PENDING_ACTIVATE");
   }
-  
 }
