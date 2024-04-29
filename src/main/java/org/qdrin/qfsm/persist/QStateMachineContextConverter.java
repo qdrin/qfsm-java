c789747d510e7260b99beaf8ffc990628281b91e
qdrin <dqdrin@gmail.com>
2024-04-29 14:06:40 +0300

    Add toJson and toContext(JsonNode) methods to QStateMachineContextConverter

--- src/main/java/org/qdrin/qfsm/persist/QStateMachineContextConverter.java
+++ src/main/java/org/qdrin/qfsm/persist/QStateMachineContextConverter.java
@@ -1,10 +1,28 @@
 package org.qdrin.qfsm.persist;
 
 import org.springframework.statemachine.StateMachineContext;
+
+import java.util.List;
+
+import org.springframework.statemachine.ExtendedState;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.kryo.StateMachineContextSerializer;
+import org.springframework.statemachine.state.AbstractState;
+import org.springframework.statemachine.state.RegionState;
+import org.springframework.statemachine.state.State;
+import org.springframework.statemachine.support.AbstractStateMachine;
+import org.springframework.statemachine.support.DefaultExtendedState;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.*;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.ValueNode;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,6 +38,86 @@ public class QStateMachineContextConverter {
     StateMachineContext<String, String> context = (StateMachineContext<String, String>) kryo.readClassAndObject(input);
     input.close();
     return context;
+  }
+
+  public static StateMachineContext<String, String> toContext(JsonNode jstate) {
+    StateMachineContext<String, String> context = null;
+    ExtendedState estate = new DefaultExtendedState();
+    String state = null;
+    switch(jstate.getNodeType()) {
+      case OBJECT:
+        state = jstate.fieldNames().next();
+        context = new DefaultStateMachineContext<>(state, null, null, estate);
+        JsonNode jchild = jstate.get(state);
+        if(jchild.getNodeType() == JsonNodeType.ARRAY) {
+          for(var j: jchild) {
+            StateMachineContext<String, String> child = toContext(j);
+            context.getChilds().add(child);
+          }
+        } else {
+          StateMachineContext<String, String> child = toContext(jchild);
+          context.getChilds().add(child);
+        }
+        break;  // ?
+      case STRING:
+        state = jstate.asText();
+        context = new DefaultStateMachineContext<>(state, null, null, estate);
+      default:
+    }
+    return context;
+  }
+
+  public static JsonNode toJsonNode(State<String, String> state) {
+    ObjectMapper mapper = new ObjectMapper();
+    // ObjectNode jcontext = mapper.createObjectNode();
+		String stateId = state.getId();
+    JsonNode result = mapper.getNodeFactory().textNode(stateId);
+
+		if (state.isOrthogonal()) {
+			RegionState<String, String> rstate = (RegionState<String, String>) state;
+      ObjectNode regions = mapper.createObjectNode();
+      ArrayNode childs = mapper.createArrayNode();
+			for(var r: rstate.getRegions()) {
+        JsonNode child = toJsonNode(r.getState());
+        childs.add(child);
+			}
+      regions.set(stateId, childs);
+      result = regions;
+		}
+		if(state.isSubmachineState()) {
+			StateMachine<String, String> submachine = ((AbstractState<String, String>) state).getSubmachine();
+			State<String, String> sstate = submachine.getState();
+      JsonNode child = toJsonNode(sstate);
+      ObjectNode subMachine = mapper.createObjectNode().set(stateId, child);
+      result = subMachine;
+		}
+    return result;
+	}
+
+  // TODO: We cannot get StateMachine context from machine or state, should we remove this method?
+  public static JsonNode toJsonNode(StateMachineContext<String, String> context) {
+    ObjectMapper mapper = new ObjectMapper();
+    String stateId = context.getState();
+    JsonNode result = mapper.getNodeFactory().textNode(stateId);
+
+    List<StateMachineContext<String, String>> childs = context.getChilds();
+    if(childs != null && ! childs.isEmpty()) {
+      if(childs.size() == 1) {
+        JsonNode childNode = toJsonNode(childs.get(0));
+        ObjectNode subMachine = mapper.createObjectNode().set(stateId, childNode);
+        result = subMachine;
+      } else {
+        ObjectNode regions = mapper.createObjectNode();
+        ArrayNode childNodes = mapper.createArrayNode();
+        for(StateMachineContext<String, String> child: childs) {
+          childNodes.add(toJsonNode(child));
+        }
+        regions.set(stateId, childNodes);
+        result = regions;
+      }
+    }
+    log.debug("JsonNode context: {}", result);
+    return result;
   }
   
   byte[] toBytes(StateMachineContext<String, String> context) {
--- src/test/java/org/qdrin/qfsm/fsm/StateMachineTest.java
+++ src/test/java/org/qdrin/qfsm/fsm/StateMachineTest.java
@@ -7,10 +7,9 @@ import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.statemachine.ExtendedState;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.StateMachineContext;
-import org.springframework.statemachine.StateMachinePersist;
 import org.springframework.statemachine.access.StateMachineAccess;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
@@ -21,21 +20,23 @@ import java.util.function.Consumer;
 
 import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.qdrin.qfsm.Helper;
 import org.qdrin.qfsm.ProductBuilder;
 import org.qdrin.qfsm.model.*;
 import org.qdrin.qfsm.persist.ProductStateMachinePersist;
+import org.qdrin.qfsm.persist.QStateMachineContextConverter;
 import org.qdrin.qfsm.tasks.ActionSuit;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.statemachine.service.StateMachineService;
-import org.springframework.statemachine.support.DefaultExtendedState;
 import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.statemachine.test.StateMachineTestPlan;
 import org.springframework.statemachine.test.StateMachineTestPlanBuilder;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.JsonNodeType;
 
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
@@ -77,6 +78,7 @@ public class StateMachineTest {
         case "Suspended":
           isProvision = true;
           stateList.addAll(Arrays.asList("UsageOn", s));
+          break;
         case "Active":
         case "ActiveTrial":
           isProvision = true;
@@ -114,37 +116,11 @@ public class StateMachineTest {
 
   ObjectMapper mapper = new ObjectMapper();
 
-  private StateMachineContext<String, String> createContext(JsonNode jstate) {
-    StateMachineContext<String, String> context = null;
-    ExtendedState estate = new DefaultExtendedState();
-    String state = null;
-    switch(jstate.getNodeType()) {
-      case OBJECT:
-        state = jstate.fieldNames().next();
-        context = new DefaultStateMachineContext<>(state, null, null, estate);
-        JsonNode jchild = jstate.get(state);
-        if(jchild.getNodeType() == JsonNodeType.ARRAY) {
-          for(var j: jchild) {
-            StateMachineContext<String, String> child = createContext(j);
-            context.getChilds().add(child);
-          }
-        } else {
-          StateMachineContext<String, String> child = createContext(jchild);
-          context.getChilds().add(child);
-        }
-        break;  // ?
-      case STRING:
-        state = jstate.asText();
-        context = new DefaultStateMachineContext<>(state, null, null, estate);
-      default:
-    }
-    return context;
-  }
-
   public StateMachine<String, String> createMachine(JsonNode machineState, String machineId) {
     if(machineState != null) {
       try {
-        StateMachineContext<String, String> context = createContext(machineState);
+        // StateMachineContext<String, String> context = createContext(machineState);
+        StateMachineContext<String, String> context = QStateMachineContextConverter.toContext(machineState);
         persist.write(context, machineId);
       } catch(Exception e) {
         log.error("cannot write context to DB: {}", e.getLocalizedMessage());
@@ -161,16 +137,98 @@ public class StateMachineTest {
     return createMachine(machineState, machineId);
   }
 
-  @Test
-  public void testContextBuilder() throws Exception {
-    ClassPathResource resource = new ClassPathResource("/contexts/machinestate_sample.json", getClass());
-    ObjectMapper mapper = new ObjectMapper();
-    JsonNode machineState = mapper.readTree(resource.getInputStream());
-    StateMachineContext<String, String> context = createContext(machineState);
-    log.debug("context: {}", context);
-    assertEquals("Provision", context.getState());
-    assertEquals(3, context.getChilds().size());
-    // assertThat(context.getExtendedState()).isNotNull();
+  @Nested 
+  class ContextConverter {
+    @Test
+    public void testContextBuilder() throws Exception {
+      ClassPathResource resource = new ClassPathResource("/contexts/machinestate_sample.json", getClass());
+      ObjectMapper mapper = new ObjectMapper();
+      JsonNode machineState = mapper.readTree(resource.getInputStream());
+      StateMachineContext<String, String> context = QStateMachineContextConverter.toContext(machineState);
+      log.debug("context: {}", context);
+      assertEquals("Provision", context.getState());
+      assertEquals(3, context.getChilds().size());
+      // assertThat(context.getExtendedState()).isNotNull();
+    }
+
+    @Test
+    public void testJsonBuilder() throws Exception {
+      ClassPathResource resource = new ClassPathResource("/contexts/machinestate_sample.json", getClass());
+      ObjectMapper mapper = new ObjectMapper();
+      JsonNode machineState = mapper.readTree(resource.getInputStream());
+      String machineId = "testJsonBuilderSource";
+      machine = createMachine(machineState, machineId);
+      JsonNode machineStateTarget = QStateMachineContextConverter.toJsonNode(machine.getState());
+      log.debug("expected: {}", machineState.toString());
+      JSONAssert.assertEquals(machineState.toString(), machineState.toString(), false);
+      JSONAssert.assertEquals(machineState.toString(), machineStateTarget.toString(), false);
+    }
+
+    // This test is just example how accessor work
+    // Unfortunately it cause exception when accessing orthogonal regions
+    // so we has gone to the persist/restore with context method
+    @Test
+    public void testSimpleStateSetByAccessor() throws Exception {
+      machine = service.acquireStateMachine("testSimpleStateSet");
+      var accessor = machine.getStateMachineAccessor();
+      StateMachineContext<String, String> context = new DefaultStateMachineContext<String,String>("Aborted", null, null, null);
+      Consumer<StateMachineAccess<String, String>> access;
+      access = arg -> {
+        arg.resetStateMachineReactively(context).block();
+      }; 
+      accessor.doWithAllRegions(access);
+
+      StateMachineTestPlan<String, String> plan =
+          StateMachineTestPlanBuilder.<String, String>builder()
+            .defaultAwaitTime(2)
+            .stateMachine(machine)
+            .step()
+                .expectState("Aborted")
+                .and()
+            .build();
+          plan.test();
+    }
+
+    @Test
+    public void testSimpleStateSet() throws Exception {
+      String machineId = "testSimpleStateSet";
+      ObjectMapper mapper = new ObjectMapper();
+      JsonNode machineState = mapper.readTree("\"Aborted\"");
+      machine = createMachine(machineState, machineId);
+      log.debug("machine: {}", machine);
+
+      StateMachineTestPlan<String, String> plan =
+          StateMachineTestPlanBuilder.<String, String>builder()
+            .defaultAwaitTime(2)
+            .stateMachine(machine)
+            .step()
+                .expectState("Aborted")
+                .and()
+            .build();
+          plan.test();
+    }
+
+    @Test
+    public void testOrthogonalStateSet() throws Exception {
+      String machineId = "testOrthogonalStateSet";
+      ClassPathResource resource = new ClassPathResource("/contexts/machinestate_sample.json", getClass());
+      ObjectMapper mapper = new ObjectMapper();
+      JsonNode machineState = mapper.readTree(resource.getInputStream());
+      machine = createMachine(machineState, machineId);
+      log.debug("machine: {}", machine);
+      log.debug("states: {}", machine.getState().getIds());
+      log.debug("search states: {}", Arrays.asList(stateSuit("Prolongation", "Paid", "PriceActive")));
+      StateMachineTestPlan<String, String> plan =
+          StateMachineTestPlanBuilder.<String, String>builder()
+            .defaultAwaitTime(2)
+            .stateMachine(machine)
+            .step()
+                .expectStates(stateSuit("Prolongation", "Paid", "PriceActive"))
+                .and()
+            .build();
+          plan.test();
+      assert(machine.getState().getIds().contains("Paid"));
+    }
   }
 
   @Test
@@ -185,52 +243,6 @@ public class StateMachineTest {
               .and()
           .build();
         plan.test();
-  }
-
-  // This test is just example how accessor work
-  // Unfortunately it cause exception when accessing orthogonal regions
-  // so we has gone to the persist/restore with context method
-  @Test
-  public void testSimpleStateSet() throws Exception {
-    machine = service.acquireStateMachine("testSimpleStateSet");
-    var accessor = machine.getStateMachineAccessor();
-    StateMachineContext<String, String> context = new DefaultStateMachineContext<String,String>("Aborted", null, null, null);
-    Consumer<StateMachineAccess<String, String>> access;
-    access = arg -> {
-      arg.resetStateMachineReactively(context).block();
-    }; 
-    accessor.doWithAllRegions(access);
-
-    StateMachineTestPlan<String, String> plan =
-        StateMachineTestPlanBuilder.<String, String>builder()
-          .defaultAwaitTime(2)
-          .stateMachine(machine)
-          .step()
-              .expectState("Aborted")
-              .and()
-          .build();
-        plan.test();
-  }
-
-  @Test
-  public void testOrthogonalStateSet() throws Exception {
-    String machineId = "testOrthogonalStateSet";
-    ClassPathResource resource = new ClassPathResource("/contexts/machinestate_sample.json", getClass());
-    ObjectMapper mapper = new ObjectMapper();
-    JsonNode machineState = mapper.readTree(resource.getInputStream());
-    machine = createMachine(machineState, machineId);
-    log.debug("machine: {}", machine);
-    StateMachineTestPlan<String, String> plan =
-        StateMachineTestPlanBuilder.<String, String>builder()
-          .defaultAwaitTime(2)
-          .stateMachine(machine)
-          // .step()
-          //     .expectStates(stateSuit("Prolongation", "Paid", "PriceActive"))
-          //     .and()
-          .build();
-        plan.test();
-    log.debug("states: {}", machine.getState().getIds());
-    assert(machine.getState().getIds().contains("Paid"));
   }
 
   @Test
