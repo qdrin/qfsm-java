8ddabbb1318553c4af7bc9f8a07a3291983ff95c
qdrin <dqdrin@gmail.com>
2024-05-06 09:04:32 +0300

    Start adding tests for custom bundle and its components

--- src/test/java/org/qdrin/qfsm/fsm/ActivationStartedTest.java
+++ src/test/java/org/qdrin/qfsm/fsm/ActivationStartedTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.*;
 import org.qdrin.qfsm.BundleBuilder;
 import org.qdrin.qfsm.Helper;
 import org.qdrin.qfsm.ProductBuilder;
+import org.qdrin.qfsm.ProductClass;
 import org.qdrin.qfsm.SpringStarter;
 import org.qdrin.qfsm.BundleBuilder.TestBundle;
 import org.qdrin.qfsm.model.*;
@@ -115,6 +116,9 @@ public class ActivationStartedTest extends SpringStarter {
         .build();
       Product product = bundle.drive;
       List<Product> components = bundle.components;
+      assertEquals(ProductClass.BUNDLE.ordinal(), product.getProductClass());
+      assertEquals(ProductClass.BUNDLE_COMPONENT.ordinal(), components.get(0).getProductClass());
+
       TestBundle expectedBundle = new BundleBuilder(bundle.products())
         .tarificationPeriod(0)
         .productStartDate(t0)
@@ -129,10 +133,7 @@ public class ActivationStartedTest extends SpringStarter {
                 .expectState("Entry")
                 .and()
             .step()
-                .sendEvent(MessageBuilder.withPayload("activation_started")
-                    .setHeader("product", product)
-                    .setHeader("datetime", OffsetDateTime.now())
-                    .build())
+                .sendEvent("activation_started")
                 .expectState("PendingActivate")
                 .expectStateChanged(1)
                 .and()
@@ -145,14 +146,20 @@ public class ActivationStartedTest extends SpringStarter {
 
     @Test
     public void testActiveSuccess() throws Exception {
-      Product product = new ProductBuilder("bundleOffer1", "", "bundle1-price-active").build();
       OffsetDateTime t0 = OffsetDateTime.now();
-      Product expectedProduct = new ProductBuilder(product)
+      TestBundle bundle = new BundleBuilder("bundleOffer1", "bundle1-price-active",
+        "component1", "component2", "component3")
         .tarificationPeriod(0)
-        .status("PENDING_ACTIVATE")
-        .productStartDate(t0)
         .build();
-      machine = createMachine(product);
+      Product product = bundle.drive;
+      List<Product> components = bundle.components;
+      TestBundle expectedBundle = new BundleBuilder(bundle.products())
+        .tarificationPeriod(0)
+        .productStartDate(t0)
+        .status("PENDING_ACTIVATE")
+        .build();
+
+      machine = createMachine(bundle);
       StateMachineTestPlan<String, String> plan =
           StateMachineTestPlanBuilder.<String, String>builder()
             .defaultAwaitTime(2)
@@ -161,55 +168,136 @@ public class ActivationStartedTest extends SpringStarter {
                 .expectState("Entry")
                 .and()
             .step()
-                .sendEvent(MessageBuilder.withPayload("activation_started")
-                    .setHeader("product", product)
-                    .setHeader("datetime", OffsetDateTime.now())
-                    .build())
+                .sendEvent("activation_started")
                 .expectState("PendingActivate")
                 .expectStateChanged(1)
                 .and()
             .build();
       plan.test();
       assertEquals(product.getStatus(), "PENDING_ACTIVATE");
-      Helper.Assertions.assertProductEquals(expectedProduct, product);
-    }    
+      Helper.Assertions.assertProductEquals(expectedBundle.drive, product);
+      Helper.Assertions.assertProductEquals(expectedBundle.components, components);
+    }
+
+    @Test
+    public void testFailAddComponent() throws Exception {
+      assert(false);
+    }
   }
 
-  @Test
-  public void testActivationCompletedSuccess() throws Exception {
-    Product product = new ProductBuilder("simpleOffer1", "", "simple1-price-trial").build();
-    ProductPrice price = product.getProductPrice().get(0);
-    price.setNextPayDate(OffsetDateTime.now().plusDays(30));
-    machine = createMachine(product);
+  @Nested
+  class CustomBundle {
+    @Test
+    public void testTrialSuccess() throws Exception {
+      OffsetDateTime t0 = OffsetDateTime.now();
+      TestBundle bundle = new BundleBuilder("customBundleOffer1", "custom1-price-trial",
+        "component1", "component2", "component3")
+        .tarificationPeriod(0)
+        .build();
+      Product product = bundle.drive;
+      List<Product> components = bundle.components;
+      assertEquals(ProductClass.CUSTOM_BUNDLE.ordinal(), product.getProductClass());
+      assertEquals(ProductClass.CUSTOM_BUNDLE_COMPONENT.ordinal(), components.get(0).getProductClass());
+      TestBundle expectedBundle = new BundleBuilder(bundle.products())
+        .tarificationPeriod(0)
+        .productStartDate(t0)
+        .status("PENDING_ACTIVATE")
+        .build();
+      machine = createMachine(bundle);
+      StateMachineTestPlan<String, String> plan =
+          StateMachineTestPlanBuilder.<String, String>builder()
+            .defaultAwaitTime(2)
+            .stateMachine(machine)
+            .step()
+                .expectState("Entry")
+                .and()
+            .step()
+                .sendEvent("activation_started")
+                .expectState("PendingActivate")
+                .expectStateChanged(1)
+                .and()
+            .build();
+      plan.test();
+      assertEquals(product.getStatus(), "PENDING_ACTIVATE");
+      Helper.Assertions.assertProductEquals(expectedBundle.drive, product);
+      Helper.Assertions.assertProductEquals(expectedBundle.components, components);
+    }
 
-    Map<Object, Object> variables = machine.getExtendedState().getVariables();
-    List<ActionSuite> actions = (List<ActionSuite>) variables.get("actions");
-    List<ActionSuite> deleteActions = (List<ActionSuite>) variables.get("deleteActions");
+    @Test
+    public void testActiveSuccess() throws Exception {
+      OffsetDateTime t0 = OffsetDateTime.now();
+      TestBundle bundle = new BundleBuilder("customBundleOffer1", "custom1-price-active",
+        "component1", "component2", "component3")
+        .tarificationPeriod(0)
+        .build();
+      Product product = bundle.drive;
+      List<Product> components = bundle.components;
+      TestBundle expectedBundle = new BundleBuilder(bundle.products())
+        .tarificationPeriod(0)
+        .productStartDate(t0)
+        .status("PENDING_ACTIVATE")
+        .build();
 
-    StateMachineTestPlan<String, String> plan =
-        StateMachineTestPlanBuilder.<String, String>builder()
-          .defaultAwaitTime(2)
-          .stateMachine(machine)
-          .step()
-              .expectState("Entry")
-              .and()
-          .step()
-              .sendEvent("activation_started")
-              .expectState("PendingActivate")
-              .expectStateChanged(1)
-              .and()
-          .step()
-              .sendEvent("activation_completed")
-              .expectStates(Helper.stateSuit("ActiveTrial", "Paid", "PriceActive"))
-              .and()
-          .defaultAwaitTime(1)
-          .build();
-    plan.test();
-    log.debug("states: {}", machine.getState().getIds());
-    log.debug("actions: {}, deleteActions: {}", actions, deleteActions);
-    assertEquals(product.getStatus(), "ACTIVE_TRIAL");
-    assert(actions.contains(ActionSuite.PRICE_ENDED));
-    assert(! actions.contains(ActionSuite.WAITING_PAY_ENDED));
-    assert(! deleteActions.contains(ActionSuite.WAITING_PAY_ENDED));
+      machine = createMachine(bundle);
+      StateMachineTestPlan<String, String> plan =
+          StateMachineTestPlanBuilder.<String, String>builder()
+            .defaultAwaitTime(2)
+            .stateMachine(machine)
+            .step()
+                .expectState("Entry")
+                .and()
+            .step()
+                .sendEvent("activation_started")
+                .expectState("PendingActivate")
+                .expectStateChanged(1)
+                .and()
+            .build();
+      plan.test();
+      assertEquals(product.getStatus(), "PENDING_ACTIVATE");
+      Helper.Assertions.assertProductEquals(expectedBundle.drive, product);
+      Helper.Assertions.assertProductEquals(expectedBundle.components, components);
+    }
+  }
+
+  @Nested
+  class CustomBundleComponent {
+    @Test
+    public void testSuccessToPendingActivate() throws Exception {
+      OffsetDateTime t0 = OffsetDateTime.now();
+      TestBundle bundle = new BundleBuilder("customBundleOffer1", "custom1-price-trial",
+        "component1", "component2")
+        .tarificationPeriod(0)
+        .build();
+
+      Product product = new ProductBuilder("component1", "", null)
+        .productClass(ProductClass.CUSTOM_BUNDLE_COMPONENT)
+        .build();
+      bundle.drive = product;
+
+      TestBundle expectedBundle = new BundleBuilder(bundle.products())
+        .tarificationPeriod(0)
+        .productStartDate(t0)
+        .status("PENDING_ACTIVATE")
+        .build();
+      machine = createMachine(bundle);
+      StateMachineTestPlan<String, String> plan =
+          StateMachineTestPlanBuilder.<String, String>builder()
+            .defaultAwaitTime(2)
+            .stateMachine(machine)
+            .step()
+                .expectState("Entry")
+                .and()
+            .step()
+                .sendEvent("activation_started")
+                .expectState("PendingActivate")
+                .expectStateChanged(1)
+                .and()
+            .build();
+      plan.test();
+      assertEquals(product.getStatus(), "PENDING_ACTIVATE");
+      Helper.Assertions.assertProductEquals(expectedBundle.drive, product);
+      Helper.Assertions.assertProductEquals(expectedBundle.bundle, bundle.bundle);
+      Helper.Assertions.assertProductEquals(expectedBundle.components, bundle.components);
+    }
   }
 }
