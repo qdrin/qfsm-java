168dfa2327e9f4ace45c23460eacb93f837b4917
qdrin <dqdrin@gmail.com>
2024-05-15 13:07:13 +0300

    Add price.productStatus check to DisconnectModeCalculator

--- src/main/java/org/qdrin/qfsm/utils/DisconnectModeCalculator.java
+++ src/main/java/org/qdrin/qfsm/utils/DisconnectModeCalculator.java
@@ -1,21 +1,29 @@
 package org.qdrin.qfsm.utils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.qdrin.qfsm.model.Characteristic;
 import org.qdrin.qfsm.model.EventProperties;
 import org.qdrin.qfsm.model.Product;
 import org.qdrin.qfsm.model.ProductCharacteristic;
+import org.qdrin.qfsm.model.ProductPrice;
 
 public class DisconnectModeCalculator {
     // TODO: Check names and values in analytics
-    private static final String eventCharName = "deactivation_mode";
+    // TODO: Add TrialDeactivationMode analysis
+
+    private static final String eventCharName = "deactivationMode";
     private static final String eventCharImmediateType = "Immediate";
     private static final String eventCharPostponedType = "Postponed";
 
-    private static final String productCharName = "deactivation_mode";
+
+    private static final Map<String, String> productCharNames = Map.of(
+        "ACTIVE_TRIAL", "TrialDeactivationMode",
+        "ACTIVE", "ActiveDeactivationMode");
     private static final String productCharImmediateType = "Immediate";
     private static final String productCharPostponedType = "Postponed";
     
@@ -27,6 +35,8 @@ public class DisconnectModeCalculator {
     public static DisconnectMode calculate(Product product, List<Characteristic> eventChars, EventProperties eventProperties) {        
 
         DisconnectMode mode = DisconnectMode.POSTPONED;
+        ProductPrice price = product.getProductPrice().get(0);
+        String productCharName = productCharNames.getOrDefault(price.getProductStatus(), productCharNames.get("ACTIVE"));
         if(product != null) {
             List<ProductCharacteristic> productChars = new ArrayList<>(product.getCharacteristic());
             Optional<ProductCharacteristic> productChar = productChars.stream()
